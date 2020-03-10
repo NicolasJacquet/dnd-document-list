95712f541d461defd795f754041bea8aa876c017
Nicolas Jacquet <nicolasjacquet89@gmail.com>
2020-03-10 13:53:33 +0100

    remove name

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,8 @@ function App() {
     <div className={style.app}>
       <header className={style.appHeader}>
         <h1 className={style.title}>
-          Yesplan
-        </h1>
-        <h2 className={style.subtitle}>
           Documents list
-        </h2>
+        </h1>
       </header>
       <main className={style.mainContent}>
         <List />
